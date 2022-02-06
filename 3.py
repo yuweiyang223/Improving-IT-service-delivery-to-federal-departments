7022e092819fa9e964eaa897115d13d268366889
Yuwei <yuwei.yang@stat.ubc.ca>
2022-02-05 18:41:47 -0800

    update 3

--- 3.py
+++ 3.py
@@ -10,3 +10,12 @@ INCIDENT_HISTORY = pd.read_csv(r'C:\Users\Yuwei\Desktop\case\INCIDENT_HISTORY.cs
 
 q3 = INCIDENT_OWNER_HISTORY.groupby('assigned_group').mean().reset_index()
 print(q3.sort_values(by='TIME_IN_STATUS_BY_OWNER_HRS', ascending= False))
+
+'''
+        assigned_group                  MTRS
+373       ITS00386                  2128.316667
+554       SM000541                  1973.583333
+221       EDC00080                  1771.750000
+476       NW000468                  1766.233333
+403       NSD00012                  1639.316667
+'''
